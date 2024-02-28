1c7845512fb1537d0e4a5559ec13ddec770068f4
vuonglt <thaivuong1503@gmail.com>
2024-02-28 14:23:11 +0700

    Update user api

--- src/pages/api/user.ts
+++ src/pages/api/user.ts
@@ -4,36 +4,54 @@ import MailTemplate from "@/app/emails/template";
 import { sendEmail } from "@/app/lib/email";
 import { render } from "@react-email/render";
 
+const doSendMail = async (user: any) => {
+  const { displayName, email, password } = user;
+  if (email) {
+    await sendEmail({
+      to: email,
+      subject: "[Rover] You have changed password!",
+      html: render(
+        MailTemplate({
+          name: displayName,
+          email: email,
+          password: password,
+        })
+      ),
+    });
+  }
+}
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  switch (req.method) {
-    case "POST":
-      const rsCreate = await createUser(req.body.user);
-      return res.status(200).json({ message: rsCreate });
-    case "PUT":
-      const rsUpdate = await updateUser(req.body.uid, req.body.user);
-      const { displayName, email, password } = req.body.user;
-      if (rsUpdate.email) {
-        await sendEmail({
-          to: email,
-          subject: "[Rover] You have changed password!",
-          html: render(
-            MailTemplate({
-              name: displayName,
-              email: email,
-              password: password,
-            })
-          ),
-        });
-      }
-
-      return res.status(200).json({ message: "Sent Successfully" });
-    case "GET":
-      const userRecord = await getUser(req.body.uid);
-      return res.status(200).json({ message: userRecord });
-    default:
-      return res.status(200).json({ message: req.method });
+  try {
+    switch (req.method) {
+      case "POST":
+        const rsCreate = await createUser(req.body.user);
+        doSendMail(req.body.user);
+        return res
+          .status(200)
+          .json({ message: "Successfully", data: { uid: rsCreate.uid } });
+      case "PUT":
+        let rsUpdate = null;
+        if (req.body.uid) {
+          rsUpdate = await updateUser(req.body.uid, req.body.user);
+        } else {
+          rsUpdate = await createUser(req.body.user);
+        }
+        doSendMail(req.body.user);
+        return res
+          .status(200)
+          .json({ message: "Successfully", data: { uid: rsUpdate.uid } });
+      case "GET":
+        const userRecord = await getUser(req.body.uid);
+        return res.status(200).json({ message: userRecord });
+      default:
+        return res.status(200).json({ message: req.method });
+    }
+  } catch(error: any) {
+    return res.status(500).json({ code: error.code, message: error.message });
   }
+  
 }
